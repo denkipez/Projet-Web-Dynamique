0ec8602afb53032e81b5f38d6073fc0e71f72068
Sami <unopossum92@gmail.com>
2022-05-23 18:20:42 +0200

    ajout connexion

---
+++ connexion.php
@@ -0,0 +1,7 @@
+<?php
+                if(isset($_GET['erreur'])){
+                    $err = $_GET['erreur'];
+                    if($err==1 || $err==2)
+                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                }
+                ?>
