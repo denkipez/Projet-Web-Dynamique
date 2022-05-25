3da7ee5fba71e51527b94ab691bb2f3726e448e6
Anais <anais.mongolo@edu.ece.fr>
2022-05-26 00:59:36 +0200

    Liaison de la page de connexion avec la bdd, modèle de la page d'acueil plus ou moins fini

--- connexion.php
+++ connexion.php
@@ -1,16 +1,38 @@
 
 <?php
+
+$login=isset($_POST["username"])? $_POST["username"]:"";
+$mdp=isset($_POST["password"])? $_POST["password"]:"";
+$nb="";
+$result="";
+
         //BDD------------------------------\\
-         $database = "omnes_sante"
-         $db_handle = mysqli_connect('localhost', 'root', '' );
+        $database = "omnessante";
+        $db_handle = mysqli_connect('localhost', 'root', '' );
         $db_found = mysqli_select_db($db_handle, $database);
-        
-        if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-        else{
+         
+        if ($db_found){
+                $sql = "SELECT COUNT(*) FROM patient WHERE Identifiant='"."$login"."' AND Mdp='"."$mdp"."'";
+                $result=mysqli_query($db_handle,$sql);
                 
+                $nb=mysqli_fetch_row($result);
+                
+                if($nb==1){
+                        session_start();
+                        $_SESSION['login'] =$login;
+                        $_SESSION['password']   =$mdp;
+                        $_SESSION['time']     = time();
+                        header('location: accueil.html');
+                        
+                }
+
+                else{ 
+                        header('Location: connexion.html');
+                        ?>  <script>
+                        alert("Identifiants et/ou mot de passe inconnu.");</script>
+                <?php
+                }
         }
+        
+
                 ?>
